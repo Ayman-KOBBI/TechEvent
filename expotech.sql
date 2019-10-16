57221a019aabe29bb32625b646b53bf77b991884
tayari16 <45538382+tayari16@users.noreply.github.com>
2019-10-16 21:11:29 +0100

    Add files via upload

--- expotech.sql
+++ expotech.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 16 oct. 2019 à 17:45
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 16 oct. 2019 à 22:09
+-- Version du serveur :  10.4.6-MariaDB
+-- Version de PHP :  7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,32 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE `commentaire` (
+  `id_comm` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `date_comm` date NOT NULL,
+  `heure_comm` time NOT NULL,
+  `Text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `events`
 --
 
-DROP TABLE IF EXISTS `events`;
-CREATE TABLE IF NOT EXISTS `events` (
-  `id_ev` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `events` (
+  `id_ev` int(11) NOT NULL,
   `id_org` int(11) NOT NULL,
-  `lieu` varchar(50) DEFAULT NULL,
+  `lieu` varchar(50) NOT NULL,
   `nb_place` int(11) NOT NULL,
   `dt_event` date NOT NULL,
-  `h_event` varchar(50) NOT NULL,
+  `h_event` time NOT NULL,
   `prix` float NOT NULL,
   `image` varchar(50) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_ev`),
-  KEY `id_org` (`id_org`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `description` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `events`
 --
 
 INSERT INTO `events` (`id_ev`, `id_org`, `lieu`, `nb_place`, `dt_event`, `h_event`, `prix`, `image`, `description`) VALUES
-(2, 1, 'mjez', 50, '2019-10-24', '08:00:00', 20, '', 'techevent'),
-(6, 5, 'ariana', 45, '2019-10-17', '10:00:00', 250, '', 'med'),
-(7, 4, 'tunis', 70, '2020-05-22', '12:30:00', 20, '', 'khayat');
+(1, 1, 'toz', 5, '2019-10-01', '09:00:00', 2.2, '', 'ahmed');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `forum`
+--
+
+CREATE TABLE `forum` (
+  `id_article` int(11) NOT NULL,
+  `id_ev` int(11) NOT NULL,
+  `id_org` int(11) NOT NULL,
+  `date_cr` date NOT NULL,
+  `heure_cr` time NOT NULL,
+  `lieu` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -58,22 +84,13 @@ INSERT INTO `events` (`id_ev`, `id_org`, `lieu`, `nb_place`, `dt_event`, `h_even
 -- Structure de la table `organisateur`
 --
 
-DROP TABLE IF EXISTS `organisateur`;
-CREATE TABLE IF NOT EXISTS `organisateur` (
-  `id_org` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `organisateur` (
+  `id_org` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `tel` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_org`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `organisateur`
---
-
-INSERT INTO `organisateur` (`id_org`, `nom`, `prenom`, `email`, `tel`) VALUES
-(1, 'med', 'khayat', 'khayat.mohamed@gmail.com', '292365424');
+  `tel` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -81,16 +98,102 @@ INSERT INTO `organisateur` (`id_org`, `nom`, `prenom`, `email`, `tel`) VALUES
 -- Structure de la table `participant`
 --
 
-DROP TABLE IF EXISTS `participant`;
-CREATE TABLE IF NOT EXISTS `participant` (
-  `id_par` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `participant` (
+  `id_par` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `tel` varchar(50) NOT NULL,
-  `solde` float NOT NULL,
-  PRIMARY KEY (`id_par`)
+  `solde` float NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id_ticket` int(11) NOT NULL,
+  `id_ev` int(11) NOT NULL,
+  `id_par` int(11) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `prenom` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD PRIMARY KEY (`id_comm`),
+  ADD KEY `id_article` (`id_article`);
+
+--
+-- Index pour la table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id_ev`),
+  ADD KEY `id_org` (`id_org`);
+
+--
+-- Index pour la table `forum`
+--
+ALTER TABLE `forum`
+  ADD PRIMARY KEY (`id_article`),
+  ADD KEY `id_ev` (`id_ev`,`id_org`,`lieu`,`image`,`description`);
+
+--
+-- Index pour la table `organisateur`
+--
+ALTER TABLE `organisateur`
+  ADD PRIMARY KEY (`id_org`);
+
+--
+-- Index pour la table `participant`
+--
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`id_par`) USING BTREE;
+
+--
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id_ticket`),
+  ADD KEY `id_ev` (`id_ev`),
+  ADD KEY `id_par` (`id_par`),
+  ADD KEY `nom` (`nom`,`prenom`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id_ev` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `organisateur`
+--
+ALTER TABLE `organisateur`
+  MODIFY `id_org` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `participant`
+--
+ALTER TABLE `participant`
+  MODIFY `id_par` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

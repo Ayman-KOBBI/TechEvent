e40850e40441c59602396ef7584b3f042c0087fe
khayat1996 <44604659+khayat1996@users.noreply.github.com>
2019-10-23 16:29:55 +0100

    tech'event

--- src/main/java/service/EventService.java
+++ src/main/java/service/EventService.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 import utils.ConnexionBD;
 import java.util.Date;
 import java.sql.Time;

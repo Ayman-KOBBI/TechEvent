226b8f87f91dac2dc98dd75d0072b5bc12e56322
khayat1996 <44604659+khayat1996@users.noreply.github.com>
2019-10-26 20:32:53 +0100

    tech'event

--- src/main/resources/fxml/ParticipantEventController.java
+++ src/main/resources/fxml/ParticipantEventController.java
@@ -5,14 +5,20 @@
  */
 package fxml;
 
+import entity.Events;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
+import service.EventService;
 
 /**
  * FXML Controller class
@@ -22,7 +28,7 @@ import javafx.scene.input.KeyEvent;
 public class ParticipantEventController implements Initializable {
 
     @FXML
-    private TableView<?> table_event;
+    private TableView table_event;
     @FXML
     private TableColumn<?, ?> nom_or;
     @FXML
@@ -47,11 +53,32 @@ public class ParticipantEventController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
         // TODO
+         ////////////////Afficher///////////
+        EventService p= new EventService();
+        ArrayList<Events> e = (ArrayList<Events>) p.affichierEvent(); 
+        ObservableList<Events> obs=FXCollections.observableArrayList(e);
+        table_event.setItems(obs);
+        nom_or.setCellValueFactory(new PropertyValueFactory<>("nom_org") );
+        nom_e.setCellValueFactory(new PropertyValueFactory<>("nom_event") );
+        lie.setCellValueFactory(new PropertyValueFactory<>("lieu") );
+        nb.setCellValueFactory(new PropertyValueFactory<>("nb_place") );
+        dt_e.setCellValueFactory(new PropertyValueFactory<>("dt_event") );
+        h_e.setCellValueFactory(new PropertyValueFactory<>("h_event") );
+        px.setCellValueFactory(new PropertyValueFactory<>("prix") );
+        desc.setCellValueFactory(new PropertyValueFactory<>("description") );
+        
+         EventService ps=new EventService();
     }    
 
     @FXML
     private void search(KeyEvent event) {
+          EventService p = new EventService();
+            String m = tfserach.getText().concat("%");
+            ArrayList<Events> e= (ArrayList<Events>) p.chercher(m);
+            ObservableList<Events> obs=FXCollections.observableArrayList(e);
+            table_event.setItems(obs);
     }
     
 }

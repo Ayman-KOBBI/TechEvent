639f38bc19bc6703bc7130d09fd494173d7d2c65
khayat1996 <44604659+khayat1996@users.noreply.github.com>
2019-10-29 02:35:19 +0100

    tech'event

--- src/main/java/utils/SmsSender.java
+++ src/main/java/utils/SmsSender.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package utils;
-
+/**
+ *
+ * @author khayat
+ */
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -14,7 +17,6 @@ public class SmsSender {
   public static final String AUTH_TOKEN = "8525b65b01da4339a55f5f7e6d6abdf8";
 
     public static void sendSms(){
-  // Find your Account Sid and Token at twilio.com/user/account
  
 
 
